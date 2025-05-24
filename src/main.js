d3c36771a9da23d1db644fbb5b3c8cdc5fad4bdc
ceosalibio <123632260+ceosalibio@users.noreply.github.com>
2025-05-24 20:22:31 +0800

    Update main.js
    remove amplify configuration

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 //import {Amplify} from 'aws-amplify';
 //import awsExports from './aws-exports';
 
-Amplify.configure(awsExports);
+//Amplify.configure(awsExports);
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
