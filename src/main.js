46976e5bebc68df2b4a83c2475e4820954807096
ceosalibio <123632260+ceosalibio@users.noreply.github.com>
2025-05-24 20:20:10 +0800

    Update main.js
    remove amplify

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import {Amplify} from 'aws-amplify';
-import awsExports from './aws-exports';
+//import {Amplify} from 'aws-amplify';
+//import awsExports from './aws-exports';
 
 Amplify.configure(awsExports);
 import { createApp } from 'vue'
